04722a32f658689339eeb7e2fee41438b34a4c77
Bartosz <bartekg001@gmail.com>
2022-11-25 13:39:38 +0100

    Fix classes of fragments.

--- StudentCrime/app/src/main/java/com/example/studentcrime/CrimesList.kt
+++ StudentCrime/app/src/main/java/com/example/studentcrime/CrimesList.kt
@@ -1,10 +1,16 @@
 package com.example.studentcrime
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
 class CrimesList : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_a, container, false)
+        return inflater.inflate(R.layout.crime_items_list, container, false)
     }
 }
--- StudentCrime/app/src/main/java/com/example/studentcrime/SpecificCrime.kt
+++ StudentCrime/app/src/main/java/com/example/studentcrime/SpecificCrime.kt
@@ -1,10 +1,17 @@
 package com.example.studentcrime
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
 class SpecificCrime : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_b, container, false)
+        return inflater.inflate(R.layout.specific_crime_item, container, false)
     }
+}
