c709ae148c15022893e3d59d8ef4971c598e13f7
Gartosz <bartekg001@gmail.com>
2022-11-26 00:35:26 +0100

    Rename class from specifc to detailed crime.

--- StudentCrime/app/src/main/java/com/example/studentcrime/DetailedCrime.kt
+++ StudentCrime/app/src/main/java/com/example/studentcrime/DetailedCrime.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class SpecificCrime : Fragment() {
+class DetailedCrime : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
