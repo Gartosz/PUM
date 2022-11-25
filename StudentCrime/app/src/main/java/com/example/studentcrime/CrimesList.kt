8c2131b1026ebdb928e7670cf775df34e60e077e
Bartosz <bartekg001@gmail.com>
2022-11-25 12:55:00 +0100

    Add 2 classes - each for new fragment.

---
+++ StudentCrime/app/src/main/java/com/example/studentcrime/CrimesList.kt
@@ -0,0 +1,10 @@
+package com.example.studentcrime
+
+class CrimesList : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+}
---
+++ StudentCrime/app/src/main/java/com/example/studentcrime/SpecificCrime.kt
@@ -0,0 +1,10 @@
+package com.example.studentcrime
+
+class SpecificCrime : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_b, container, false)
+    }
