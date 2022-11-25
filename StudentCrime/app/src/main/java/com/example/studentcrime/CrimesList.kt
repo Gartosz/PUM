55853d1b6cdbf5f3eca9bfe117227aebf2529340
Bartosz <bartekg001@gmail.com>
2022-11-25 22:42:29 +0100

    RecyclerView operating on first fragment class.

--- StudentCrime/app/src/main/java/com/example/studentcrime/CrimesList.kt
+++ StudentCrime/app/src/main/java/com/example/studentcrime/CrimesList.kt
@@ -5,12 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class CrimesList : Fragment() {
+    private var crimesList =  MutableList(50) { "Crime #$it" }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.crime_items_list, container, false)
     }
-}
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = ListAdapter(crimesList)
+        }
+    }
