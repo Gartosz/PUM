fb0230f36ef7bb1bd6050681d9bfe5214dcdfb14
Gartosz <bartekg001@gmail.com>
2022-11-27 19:54:36 +0100

    Move both fragments to fragments directory.

--- StudentCrime/app/src/main/java/com/example/studentcrime/fragments/DetailedCrime.kt
+++ StudentCrime/app/src/main/java/com/example/studentcrime/fragments/DetailedCrime.kt
@@ -1,10 +1,11 @@
-package com.example.studentcrime
+package com.example.studentcrime.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.studentcrime.R
 
 class DetailedCrime : Fragment() {
 
