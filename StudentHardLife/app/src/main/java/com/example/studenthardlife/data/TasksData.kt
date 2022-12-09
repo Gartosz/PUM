0ff729910778f02b78228c395fb197e289b4f412
Gartosz <>
2022-12-09 23:20:48 +0100

    Add elements to object lists.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/data/TasksData.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/data/TasksData.kt
@@ -1,6 +1,6 @@
 package com.example.studenthardlife.data
 
 object TasksData {
-    private var titles = listOf<String>()
-    private var contents = listOf<String>()
+    private var titles = listOf<String>("Lista 1", "Lista 2")
+    private var contents = listOf<String>("Zadanie 1", "Zadanie 1")
 }
