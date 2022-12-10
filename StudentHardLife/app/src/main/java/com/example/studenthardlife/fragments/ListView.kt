cf2506ab5df6b4f162f8aa365ac0e476944e2889
Gartosz <>
2022-12-10 14:02:05 +0100

    Adapt fragment classes from previous task to work in that one.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/fragments/ListView.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/fragments/ListView.kt
@@ -1,7 +1,38 @@
 package com.example.studenthardlife.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.studenthardlife.R
+import java.lang.IllegalArgumentException
 
 class ListView : Fragment(){
+    private val index by lazy {
+        arguments?.getInt("index")?:
+        throw IllegalArgumentException("index doesn't exist")
+    }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_view, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        try {
+
+        }
+        catch (e: Exception){
+            println(e)
+        }
+
+    }
 }
--- StudentHardLife/app/src/main/java/com/example/studenthardlife/fragments/TasksLists.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/fragments/TasksLists.kt
@@ -1,7 +1,30 @@
 package com.example.studenthardlife.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studenthardlife.R
+import com.example.studenthardlife.adapters.ListAdapter
+import com.example.studenthardlife.data.TasksData
 
 class TasksLists : Fragment() {
+    private var lists = TasksData.getLists()
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.tasks_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = ListAdapter(lists)
+    }
 }
