76b454d913c57d6d2c9f5f0c605b53aebf2c92f4
Gartosz <>
2022-12-16 09:57:13 +0100

    Create sharedpreferences save on pause.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/MainActivity.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.studenthardlife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.studenthardlife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val sharedPref = getSharedPreferences("fileName", MODE_PRIVATE)
+        val titles = mutableSetOf("A", "B")
+        val edit = sharedPref.edit()
+        edit.apply {
+            putStringSet("titles", titles)
+            apply()
+        }
     }
 }
