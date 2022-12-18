c9a7d56c5acf55879478b2889f60b68de0ebb7ea
Gartosz <bartekg001@gmail.com>
2022-12-19 00:05:45 +0100

    Change list of lists to mutable.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/data/TasksData.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/data/TasksData.kt
@@ -1,15 +1,15 @@
 package com.example.studenthardlife.data
 
 object TasksData {
-    private lateinit var listsElements: List<ListElement>
+    private lateinit var listsElements: MutableList<ListElement>
 
     fun getLists(): List<ListElement>
     {
-        return listsElements
+        return listsElements.toList()
     }
 
     fun importLists(list: List<ListElement>)
     {
-        listsElements = list
+        listsElements = list.toMutableList()
     }
 }
