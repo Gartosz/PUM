d97d9fbfbcc56de0891e689918ff4a3ea586df2a
Gartosz <bartekg001@gmail.com>
2022-12-18 23:52:20 +0100

    Remove variables initialization from object.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/data/TasksData.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/data/TasksData.kt
@@ -1,9 +1,7 @@
 package com.example.studenthardlife.data
 
 object TasksData {
-    private var titles = listOf<String>("Lista 1", "Lista 2")
-    private var contents = listOf<String>("Zadanie 1", "Zadanie 1")
-    private var listsElements = listOf<ListElement>(ListElement(titles[0]), ListElement(titles[1]))
+    private lateinit var listsElements: List<ListElement>
 
     fun getLists(): List<ListElement>
     {
