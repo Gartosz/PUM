d88b2ea9590b402a24d291414f85258046d1c910
Gartosz <bartekg001@gmail.com>
2022-12-20 17:24:51 +0100

    Add content to data class with list.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/data/ListElement.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/data/ListElement.kt
@@ -1,6 +1,7 @@
 package com.example.studenthardlife.data
 
-data class ListElement(var title_: String)
+data class ListElement(var title_: String, var content_: String = "")
 {
     var title: String = title_
+    var content: String = content_
 }
