c436604a4d97276c05586e78a6076121e74a4ef4
Gartosz <bartekg001@gmail.com>
2022-12-20 22:14:48 +0100

    Get title of new list from user.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/fragments/TasksLists.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/fragments/TasksLists.kt
@@ -1,9 +1,14 @@
 package com.example.studenthardlife.fragments
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -32,7 +37,30 @@ class TasksLists : Fragment() {
         addListButton = view.findViewById(R.id.addButton)!!
         addListButton.setOnClickListener()
         {
-            
+            newListTitle()
         }
+    }
+
+    private fun newListTitle() {
+        val alertDialog = AlertDialog.Builder(context)
+
+        val input = EditText(context)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+
+        alertDialog.apply {
+            setTitle("Set Title")
+            setView(input)
+            setPositiveButton("Create") { _: DialogInterface?, _: Int ->
+                val title = input.text.toString()
+                if (title != "")
+                    TasksData.addList(input.text.toString())
+                else
+                    Toast.makeText(context, "Title can't be empty!", Toast.LENGTH_SHORT).show()
+            }
+            setNegativeButton("Cancel") { dialog, _ ->
+                dialog.cancel()
+            }
+
+        }.create().show()
     }
 }
