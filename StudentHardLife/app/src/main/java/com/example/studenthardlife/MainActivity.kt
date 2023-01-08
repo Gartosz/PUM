0c0dbbf02a351bf2de947831eb86c84dcfce153a
Bartosz <bartekg001@gmail.com>
2023-01-08 14:57:48 +0100

    Add binding to main activity.

--- StudentHardLife/app/src/main/java/com/example/studenthardlife/MainActivity.kt
+++ StudentHardLife/app/src/main/java/com/example/studenthardlife/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.studenthardlife
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studenthardlife.data.TasksData
+import com.example.studenthardlife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TasksData.setupSharedPreferences(this)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 
     override fun onPause() {
