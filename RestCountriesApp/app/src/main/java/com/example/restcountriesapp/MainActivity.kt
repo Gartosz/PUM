edd862e0a94bfc437b422fc95fee5e07fa55bc6c
Gartosz <bartekg001@gmail.com>
2023-01-15 18:24:46 +0100

    Add nav controller.

--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/MainActivity.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.restcountriesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
     }
 }
