517ce2fd5099e136d2acd7201e4efd6ef7faba69
Gartosz <bartekg001@gmail.com>
2023-01-15 15:21:58 +0100

    Set Fragment as base class for fragments.

--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/fragments/CapitalsFragment.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/fragments/CapitalsFragment.kt
@@ -1,4 +1,6 @@
 package com.example.restcountriesapp.fragments
 
-class CapitalsFragment {
+import androidx.fragment.app.Fragment
+
+class CapitalsFragment : Fragment() {
 }
--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/fragments/FlagsFragment.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/fragments/FlagsFragment.kt
@@ -1,4 +1,6 @@
 package com.example.restcountriesapp.fragments
 
-class FlagsFragment {
+import androidx.fragment.app.Fragment
+
+class FlagsFragment : Fragment() {
 }
