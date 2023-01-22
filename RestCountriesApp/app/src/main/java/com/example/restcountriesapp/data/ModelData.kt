943fa8c1c48d5ecb4132de1e249ee8dc5030db59
Gartosz <>
2023-01-23 00:02:42 +0100

    Change model class to accept string with link to api on initialization.

--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/ModelData.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/ModelData.kt
@@ -1,8 +1,10 @@
 package com.example.restcountriesapp.data
 
-data class ModelData (private val countries: List<String>, private val capitals: List<String>) {
+data class ModelData (private val apiLink: String) {
+    private lateinit var countries: List<String>
+    private lateinit var capitals: List<String>
 
-    fun getCapitals(): Map<String, String>
+    fun getCapitals(): Any
     {
         return countries.zip(capitals).toMap()
     }
