86213f37469eff8a5297fddb1433e2899e2d88a1
Gartosz <>
2023-01-23 20:12:23 +0100

    Add strings with fields.

--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/data.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/data.kt
@@ -1,5 +1,7 @@
 package com.example.restcountriesapp.data
 
 object data {
-    var api_link: String = "https://restcountries.com/v3.1/all"
+    val api_link = "https://restcountries.com/v3.1/all"
+    val names_capitals = "?fields=name,capital"
+    val names_flags = "?fields=name,flags"
 }
