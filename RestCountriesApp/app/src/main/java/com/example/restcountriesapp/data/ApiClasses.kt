cb1796a48c76c15b1128285a2b12ae5421b88c8f
Gartosz <>
2023-01-28 00:36:56 +0100

    Remove unnecessary values from JSON data class.

--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/ApiClasses.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/ApiClasses.kt
@@ -1,23 +1,11 @@
 class CountriesResponse : ArrayList<CountriesJSONData>()
 
 data class CountriesJSONData(
-    val altSpellings: List<String>,
     val capital: List<String>,
     val flag: String,
     val name: Name
 )
 
 data class Name(
-    val common: String,
-    val nativeName: NativeName,
-    val official: String
-)
-
-data class NativeName(
-    val eng: Eng
-)
-
-data class Eng(
-    val common: String,
-    val official: String
+    val common: String
 )
