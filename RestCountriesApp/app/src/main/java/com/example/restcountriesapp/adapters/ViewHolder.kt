284fa64e24b811f09e51645fc1b06ca627f35a3c
Gartosz <>
2023-01-27 23:55:22 +0100

    Implement bins method to view holder.

--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/adapters/ViewHolder.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/adapters/ViewHolder.kt
@@ -1,8 +1,13 @@
 package com.example.restcountriesapp.adapters
 
+import CountriesJSONData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.restcountriesapp.databinding.ItemBinding
 
 class ViewHolder(private val binding: ItemBinding)
     : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: CountriesJSONData){
+        binding.countryName.text = item.name.toString()
+        binding.countryElement.text = item.capital.toString()
+    }
 }
