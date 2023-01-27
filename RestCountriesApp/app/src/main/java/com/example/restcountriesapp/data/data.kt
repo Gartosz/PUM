9d348431301b44d539b1f64ebc40dd92b0b8b4ac
Gartosz <>
2023-01-27 13:37:04 +0100

    Change api link.

--- RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/data.kt
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/data/data.kt
@@ -1,6 +1,6 @@
 package com.example.restcountriesapp.data
 
 object data {
-    val api_link = "https://restcountries.com/v3.1/all"
-    const val endpoint = "?fields=name,capital,flag"
+    val api_link = "https://restcountries.com"
+    const val endpoint = "v3.1/all?fields=name,capital,flag"
 }
