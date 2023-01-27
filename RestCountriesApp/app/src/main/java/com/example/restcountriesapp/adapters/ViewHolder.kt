b61470065f3612961a3efbe3c13ecdac27f0c553
Gartosz <>
2023-01-27 19:20:21 +0100

    Create viewholder.

---
+++ RestCountriesApp/app/src/main/java/com/example/restcountriesapp/adapters/ViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.restcountriesapp.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.restcountriesapp.databinding.ItemBinding
+
+class ViewHolder(private val binding: ItemBinding)
+    : RecyclerView.ViewHolder(binding.root) {
+}
