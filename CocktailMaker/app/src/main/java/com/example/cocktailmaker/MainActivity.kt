283ca008da20131b3fea2d74e370ffdcb0a4a6f9
Gartosz <>
2023-02-04 18:18:18 +0100

    Update main activity to support fragments and bottom navigation.

--- CocktailMaker/app/src/main/java/com/example/cocktailmaker/MainActivity.kt
+++ CocktailMaker/app/src/main/java/com/example/cocktailmaker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.cocktailmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cocktailmaker.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        bottomNav?.setupWithNavController(navController)
     }
 }
