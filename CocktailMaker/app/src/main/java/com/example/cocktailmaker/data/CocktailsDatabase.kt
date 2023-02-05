82f0689758589585fbb4b323b0da257abbf91a9e
Gartosz <>
2023-02-05 17:50:44 +0100

    Create database class.

---
+++ CocktailMaker/app/src/main/java/com/example/cocktailmaker/data/CocktailsDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.cocktailmaker.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.cocktailmaker.models.Ingredient
+import com.example.cocktailmaker.models.Cocktail
+
+@Database(entities = [Ingredient::class, Cocktail::class], version = 1, exportSchema = false)
+abstract class CocktailsDatabase : RoomDatabase() {
+    abstract fun ingredientDao(): IngredientDAO
+    abstract fun cocktailDao(): CocktailDAO
+
+    companion object{
+        @Volatile private var INSTANCE: CocktailsDatabase? = null
+
+        fun getDatabase(context: Context): CocktailsDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CocktailsDatabase::class.java,
+                    "cocktails_database"
+                ).build().also { INSTANCE = it }
+                instance
+            }
+        }
+    }
+}
