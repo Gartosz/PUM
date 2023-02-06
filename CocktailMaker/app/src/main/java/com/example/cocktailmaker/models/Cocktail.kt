5ad7cca48a9546d44b7da42b45669c7d9d63a3c9
Gartosz <>
2023-02-06 01:32:02 +0100

    Add primary keys.

--- CocktailMaker/app/src/main/java/com/example/cocktailmaker/models/Cocktail.kt
+++ CocktailMaker/app/src/main/java/com/example/cocktailmaker/models/Cocktail.kt
@@ -1,9 +1,11 @@
 package com.example.cocktailmaker.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "cocktails_table")
 data class Cocktail (
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String,
     val ingredients: List<String>,
--- CocktailMaker/app/src/main/java/com/example/cocktailmaker/models/Ingredient.kt
+++ CocktailMaker/app/src/main/java/com/example/cocktailmaker/models/Ingredient.kt
@@ -1,9 +1,11 @@
 package com.example.cocktailmaker.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "ingredients_table")
 data class Ingredient (
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String
 )
